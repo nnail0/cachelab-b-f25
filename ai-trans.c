b40851fc8cc88dd9edb0c9f2542f927156680a43
Nathan Nail <nnail0@unm.edu>
2025-11-18 12:01:55 -0700

    adding files to repo

---
+++ ai-trans.c
@@ -0,0 +1,10 @@
+/*********
+ * Roxanne Lutz, Nathan Nail
+ * CS 341L Fall 2025
+ * 
+ * ai-trans.c: version that utilizes the help of AI
+ * to try and transpose with minimal cache misses. 
+ * 
+ * 
+ * 
+ */
---
+++ trans-human.c
@@ -0,0 +1,10 @@
+/*********
+ * Roxanne Lutz, Nathan Nail
+ * CS 341L Fall 2025
+ * 
+ * 
+ * trans-human.c: transpose implemented independently. 
+ * 
+ * 
+ * 
+ */
